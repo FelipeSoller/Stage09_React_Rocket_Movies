b0e48eef493c684ea973ef1cc4580810029f1475
Felipe Soller <soller@outlook.com>
2024-01-17 09:54:40 -0300

    create Profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,60 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
+
+import { Container, Form, Avatar } from "./styles";
+
+export const Profile = () => {
+  return (
+    <Container>
+      <header>
+        <ButtonText title="Back" icon={FiArrowLeft} />
+      </header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/felipesoller.png"
+            alt="User avatar"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input
+              id="avatar"
+              type="file"
+            />
+          </label>
+        </Avatar>
+
+        <Input
+          placeholder="Name"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Current password"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="New password"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Save" />
+      </Form>
+    </Container>
+  )
+}
